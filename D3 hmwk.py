a83671fe02684c49beed1e5e40c60c7fbd938ad9
abdelkaderelhamri <abdelmagid621@gmail.com>
2023-03-26 18:27:45 -0400

    3rd commit

--- D3 hmwk.py
+++ D3 hmwk.py
@@ -1,13 +1,5 @@
 #1) Build a Shopping Cart
 # You can use either lists or dictionaries. The program should have the following capabilities:
-
-# 1) Takes in input
-# 2) Stores user input into a dictionary or list
-# 3) The User can add or delete items
-# 4) The User can see current shopping list
-# 5) The program Loops until user 'quits'
-# 6) Upon quiting the program, print out all items in the user's list
-
 shopping_cart = {} #decleared an empty shopping cart to 
 
 def add_item():
@@ -47,6 +39,7 @@ while True:
         show_shopping_cart()
         
     elif user_choice.lower() == "q":
+        show_shopping_cart()
         print("thank you for shopping with us")
         break
     else: 
